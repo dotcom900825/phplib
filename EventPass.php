af84983364cd8c035cd9d41734789f7f9932569d
qdx <qdxzzz@gmail.com>
2013-08-04 00:17:12 -0700

    test git, idea and git hooks

--- EventPass.php
+++ EventPass.php
@@ -16,7 +16,7 @@ require_once ("DebugLog.php");
 class EventPass extends Pass
 {
 
-    /* Pass configuration is bundled in the class */
+    /* Pass configuration is bundled in the class test*/
 
     // relative path to the folder where your certificate and key files are
     public $keyPath;
