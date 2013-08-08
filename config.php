a6bc73a239a14f272f5d1256d677e9123be84a9b
qdx <qdxzzz@gmail.com>
2013-08-07 23:25:08 -0600

    untrack config.php for database specific configwq

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-class configs{
-    public static $dbname = "iPassStore";
-    public static $user = "root";
-    public static $pass = "qidexin69";
-    public static $host = "localhost";
-    public static $instance = null;
-
-}
